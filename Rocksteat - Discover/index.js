f78bb9bacf172e4ee7e26f754c1645076b68dd0e
David Rodrigues <david152012@hotmail.com>
2021-07-09 14:58:24 -0300

    App BestMe

--- Rocksteat - Discover/index.js
+++ Rocksteat - Discover/index.js
@@ -1,1 +1,46 @@
-console.log("Rodando via script")
+//console.log("Rodando via script")
+
+const questions = [
+    "O que eu aprendi hoje?",
+    "O que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas ajudei hoje?"
+]
+
+const ask =(index = 0) => {
+    return process.stdout.write(questions[index]+"\n")
+}
+
+ask()
+
+const answers = []
+process.stdin.on("data", data =>{
+    answers.push(data.toString().trim())
+    if(answers.length < questions.length){
+        ask(answers.length)
+    }else{
+        console.log(answers)
+        process.exit()
+    }
+    
+})
+
+process.on("exit", () =>{
+    console.log(
+        `Legal David Rodrigues!
+
+        O que você aprendeu hoje foi: ${answers[0]}
+
+        O que te aborreceu e poderia melhorar: ${answers[1]}
+
+        O que te deixou feliz hoje: ${answers[2]}
+
+        Você ajudou ${answers[3]} pessoas hoje.
+
+        Volte amanhã para novas reflexões!
+
+        `
+    
+
+    )
+})
