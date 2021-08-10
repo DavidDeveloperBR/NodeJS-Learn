79a9108daee9ba9dd9b32a67bd058ab5157911c9
David Rodrigues <david152012@hotmail.com>
2021-08-10 16:02:08 -0300

    1 dia curso Udemy

---
+++ Inicio/app.js
@@ -0,0 +1,38 @@
+var calculadora = require("./calculadora");
+
+
+
+
+console.log(calculadora.mult(80,20));
+console.log(calculadora.soma(80,20));
+console.log(calculadora.sub(80,20));
+console.log(calculadora.div(80,20));
+console.log(calculadora.nome);
+
+calculadora.nome ="Calculadora do David"
+
+console.log(calculadora.nome)
+
+
+
+
+
+
+
+
+
+
+
+/*var mostrarSite = true;
+
+const site = "www.guiadoprogramador.com"
+
+console.log("Hello World!");
+console.log("Meu nome é David!")
+console.log("E eu estou aprendendo NOde.js com o Guia do Progaramador")
+
+if(mostrarSite){ // se mostrar site 
+    console.log(site) // print site
+}
+
+*/
