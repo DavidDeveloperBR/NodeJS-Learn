04de7d6aed76fdd6b8bb94ebc6a3ee4af9fce3ea
David Rodrigues <david152012@hotmail.com>
2022-02-03 20:19:07 -0300

    Table e campos virtuais

--- GuiaConverter/app.js
+++ GuiaConverter/app.js
@@ -1,15 +1,24 @@
 var Reader = require('./Reader');
+var Processor = require('./Processor');
+var Table = require('./Table')
+
 
 
 var leitor = new Reader();
 
-
-
 async function main(){
 
     let dados = await leitor.Read("./users.csv");
-    console.log(dados);
+    
+    var dadosProcessados = Processor.Process(dados);
 
+    var usuarios = new Table(dadosProcessados);
+
+    usuarios.rows.push(["George Soros", "Soros Company", "2"])
+    // console.log(usuarios.header);
+    // console.log(usuarios.rows);
+    console.log(usuarios.RowCount);
+    console.log(usuarios.ColumnCount);
 }
 
 main();
