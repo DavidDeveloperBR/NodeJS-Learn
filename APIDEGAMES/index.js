51944a2a7195e8ea83ee871104c68c01326ce794
David Rodrigues <david152012@hotmail.com>
2022-02-11 19:10:58 -0300

    API com Express

---
+++ APIDEGAMES/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use
+
+let DB = {
+    games: [
+        {
+            id: 1,
+            title: "God Of War",
+            ano: 2019,
+            preco: 200
+        },
+
+        {
+            id: 34,
+            title: "Lost Ark",
+            ano: 2022,
+            preco: 0
+        },
+        {
+            id: 101,
+            title: "Horizon Zero Down",
+            ano: 2018,
+            preco: 150
+        }
+
+    ]
+}
+
+app.get("/games",(req, res)=>{
+    res.statusCode = 200;
+    res.json(DB.games);
+});
+
+app.get("/game/:id", (req,res)=>{
+
+    if(isNaN(req.params.id)){
+        //res.send("Isso não é um número!");
+        res.sendStatus(400);
+    }else{
+        let id = parseInt(req.params.id);
+
+        let game = DB.games.find(g => g.id == id);
+
+        if(game == undefined){
+            res.sendStatus(404);
+        }else{
+            res.statusCode = 200;
+            res.json(game);
+        }
+
+    }
+    
+
+    res.json(DB.games.id == id);
+});
+
+app.listen(45678, ()=>{
+    console.log("API RODANDO...");
+})
