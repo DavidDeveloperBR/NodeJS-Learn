4270b593e5ab95249ad540478c6097fc02a3ef27
David Rodrigues <david152012@hotmail.com>
2022-03-08 18:51:03 -0300

    Relacionamentos e Inserts associados

--- BANCO/index.js
+++ BANCO/index.js
@@ -58,8 +58,42 @@ database.select()
     })
 */
 
+/*
 database.raw("SELECT * FROM games").then(data => {
     console.log(data);
 }).catch(err =>{
+    console.log(err);
+});
+*/
+
+/* DELETE
+database.where({id: 3}).delete().table("games").then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
+*/
+/* UPDATE
+database.where({id: 5}).update({preco: 70}).table("games").then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
+*/
+
+/* ORDER BY
+database.select().table("games").orderBy("nome", "asc").then(data =>{
+        console.log(data);
+}).catch(err => {
+    console.log(err);
+}) //desc //asc 
+*/
+
+database.insert({
+    nome: "CD Projekt",
+    gameid: 1
+}).table("estudios").then(data => {
+    console.log(data);
+}).catch(err => {
     console.log(err);
 })
