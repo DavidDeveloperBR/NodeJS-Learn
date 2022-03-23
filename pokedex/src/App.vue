83296b1b465c35c4f7a9d80b3af6c0d3de92e745
David Rodrigues <david152012@hotmail.com>
2022-03-23 19:44:11 -0300

    Pokedex com VueJS

---
+++ pokedex/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
+export default {
+  name: 'App',
+  data() {
+    return{
+      pokemons: []
+    }
+  },
+  created: function () {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res=> { 
+      console.log("Lista de Pokemons");
+      this.pokemons = res.data.results;
+    }).catch(err =>{
+      console.log(err);
+    })
+  },
+  components:{
+    Pokemon
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ pokedex/src/components/Pokemon.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>{{num}} {{upper}}</h1>
+    <small>{{url}}</small>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created: function(){
+        axios.get(this.url).then(res =>{
+            this.pokemon.type = res.data.types[0].type.name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            console.log(this.pokemon);
+        }).catch(err =>{
+            console.log(err);
+        });
+    },
+    data() {
+        return{
+            pokemon: {}
+        }
+    },
+  props:{
+    num: Number,
+    name: String,
+    url: String
+  },
+  computed: {
+      upper: function(){
+          return this.name[0].toUpperCase() + this.name.slice(1);
+      }
+  }
+};
+</script>
+
+<style>
+</style>
