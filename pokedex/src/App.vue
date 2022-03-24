6e9d826e7b38158b0b8bc06275c70930fed9ddeb
David Rodrigues <david152012@hotmail.com>
2022-03-24 19:34:58 -0300

    Finalizando módulo de Pokedex

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,34 +1,69 @@
 <template>
+
   <div id="app">
-    <div v-for="(poke,index) in pokemons" :key="index">
-      <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/Pokemon-Logo.png">
+      <hr>
+      <h4 class="is-size-4">POKEDEX</h4>
+      <input type="text" class="input is-rounded"  v-model="busca" placeholder="Buscar Pokemon pelo nome:">
+      <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke, index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import Pokemon from './components/Pokemon';
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 export default {
-  name: 'App',
+  name: "App",
   data() {
-    return{
-      pokemons: []
-    }
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
+    };
   },
   created: function () {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res=> { 
-      console.log("Lista de Pokemons");
-      this.pokemons = res.data.results;
-    }).catch(err =>{
-      console.log(err);
-    })
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log("Lista de Pokemons");
+        this.pokemons = res.data.results;
+        this.filteredPokemons = res.data.results;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
-  components:{
-    Pokemon
+  components: {
+    Pokemon,
+  },
+  methods:{
+    buscar: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.busca == "" || this.busca == " "){
+        this.filteredPokemons = this.pokemons;
+      }else{
+        this.filteredPokemons = this.pokemons
+          .filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+  computed: {
+    /*
+    resultadoBusca: function(){
+      if(this.busca == "" || this.busca == " "){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+    */
   }
-
-}
+};
 </script>
 
 <style>
@@ -39,5 +74,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#buscaBtn{
+  margin-top: 2%;
 }
 </style>
--- pokedex/src/components/Pokemon.vue
+++ pokedex/src/components/Pokemon.vue
@@ -1,41 +1,83 @@
 <template>
-  <div>
-    <h1>{{num}} {{upper}}</h1>
-    <small>{{url}}</small>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="currentImg" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left"></div>
+          <div class="media-content">
+            <p class="title is-4">{{ num }} - {{ upper }}</p>
+            <p class="subtitle is-6">{{ pokemon.type }}</p>
+          </div>
+        </div>
+
+        <div class="content">
+          <button class="button is-medium is-fullwidth" @click="mudarSprite">Mudar Sprite</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    created: function(){
-        axios.get(this.url).then(res =>{
-            this.pokemon.type = res.data.types[0].type.name;
-            this.pokemon.front = res.data.sprites.front_default;
-            this.pokemon.back = res.data.sprites.back_default;
-            console.log(this.pokemon);
-        }).catch(err =>{
-            console.log(err);
-        });
-    },
-    data() {
-        return{
-            pokemon: {}
-        }
-    },
-  props:{
+  created: function () {
+    axios
+      .get(this.url)
+      .then((res) => {
+        this.pokemon.type = res.data.types[0].type.name;
+        this.pokemon.front = res.data.sprites.front_default;
+        this.pokemon.back = res.data.sprites.back_default;
+        this.currentImg = this.pokemon.front;
+        console.log(this.pokemon);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  data() {
+    return {
+      isFront: true,
+      currentImg: '',
+      pokemon: {
+        type: "",
+        front: "",
+        back: "",
+      },
+    };
+  },
+  props: {
     num: Number,
     name: String,
-    url: String
+    url: String,
   },
   computed: {
-      upper: function(){
-          return this.name[0].toUpperCase() + this.name.slice(1);
+    upper: function () {
+      return this.name[0].toUpperCase() + this.name.slice(1);
+    },
+  },
+  methods: {
+    mudarSprite: function(){
+      if(this.isFront){
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      }else{
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
       }
+    }
   }
 };
 </script>
 
 <style>
+#pokemon {
+  margin-top: 2%;
+}
 </style>
